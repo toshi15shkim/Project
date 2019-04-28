077e25e851ed7ca0175941270624b77ac7d14456
toshi15shkim <toshi15shkim@gmail.com>
2019-04-28 23:29:31 +0900

    Module Commit

--- apartServerNodeJS/server.js
+++ apartServerNodeJS/server.js
@@ -1,14 +1,18 @@
+var express = require('express');
 var AWS = require('aws-sdk');
-var http = require('http');
+var app = express();
+var http = require('http').Server(app);
 
 //base경로
 global.__base = __dirname + '/';
 
-http.createServer(function handler(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var cm = require(__base+'module/common');
+
+//http.createServer(function handler(req, res) {
+//    res.writeHead(200, {'Content-Type': 'text/plain'});
+//    res.end('Hello World\n');
+//}).listen(1337, '127.0.0.1');
+//console.log('Server running at http://127.0.0.1:1337/');
 
 AWS.config.update({
 	  region: "ap-northeast-2"
@@ -25,6 +29,9 @@ dynamodb.describeTable(param, function(err, data) {
 		console.log("Data " + JSON.stringify(data));
 	}
 });
+
+http.listen(8080);
+cm.logger.debug("Server Start on port 8080");
 
 /*var param = {
 		TableName : "users",
