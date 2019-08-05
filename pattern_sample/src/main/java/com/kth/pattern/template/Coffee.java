16d8689567df739b4f747ce492613d219d72c61d
toshi15shkim <toshi15shkim@gmail.com>
2019-08-05 18:02:37 +0900

    클래스 일부 수정

--- pattern_sample/src/main/java/com/kth/pattern/template/Coffee.java
+++ pattern_sample/src/main/java/com/kth/pattern/template/Coffee.java
@@ -1,10 +1,13 @@
 package com.kth.pattern.template;
 
 public class Coffee extends CaffeineBeverage {
+
+    @Override
     public String brew() {
         return "필터로 커피를 우려내는 중";
     }
 
+    @Override
     public String addCondiments() {
         return "설탕과 커피를 추가하는 중";
     }
--- pattern_sample/src/main/java/com/kth/pattern/template/Tea.java
+++ pattern_sample/src/main/java/com/kth/pattern/template/Tea.java
@@ -1,10 +1,13 @@
 package com.kth.pattern.template;
 
 public class Tea extends CaffeineBeverage {
+
+    @Override
     public String brew() {
         return "차물 우려내는 중";
     }
 
+    @Override
     public String addCondiments() {
         return "레몬을 추가하는 중";
     }
